09a060a9a23d3ddf6a8748f28727d8766cf87796
ram24 <ram24@gmail.com>
2025-07-17 15:52:41 +0530

    amplify code

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from AWS Amplify!</h1>
+      <p>This is a simple React app hosted on Amplify.</p>
+    </div>
+  );
+}
+
+export default App;
